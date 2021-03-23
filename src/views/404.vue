796f954c884ad5d6d9432f27fdad5dc02bd602c8
TFF2060 <770425549@qq.com>
2021-03-23 13:44:05 +0800

    update: 404-1

--- src/views/404.vue
+++ src/views/404.vue
@@ -23,7 +23,7 @@
 <script>
 
 export default {
-  name: 'Page4041213333322222',
+  name: 'Page404',
   computed: {
     message() {
       return 'The webmaster said that you can not enter this page...'
